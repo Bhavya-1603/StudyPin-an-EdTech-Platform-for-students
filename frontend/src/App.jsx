ca2894d054151bbfe27d885fa8932f8c722b0e86
Bhavya-1603 <yeahbhav456@gmail.com>
2026-06-07 10:09:05 +0530

    modified App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
-import AppRoutes from './routes/AppRoutes'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./pages/Login"
 
-function App() {
-  return <AppRoutes />
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App
